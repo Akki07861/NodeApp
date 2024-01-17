29b6e5e36606d3d20b8c9d12a1c433906ca26422
Akki07861 <155167003+Akki07861@users.noreply.github.com>
2024-01-17 13:29:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world');
+ res.send('hello worlds');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 module.exports = app;
