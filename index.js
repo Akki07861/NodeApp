0467d3663c4680f078a5604c026e2d3e92e4ea27
Akki07861 <155167003+Akki07861@users.noreply.github.com>
2024-01-17 14:53:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express'); 
-
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
  res.send('hello world');
